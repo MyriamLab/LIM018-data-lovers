6ff132e6b527458256d47777aa012d449b4616a3
Myriam <myriam.rivera.santiago@gmail.com>
2022-06-17 17:30:29 -0500

    se agrega chart.js

--- src/data.js
+++ src/data.js
@@ -2,89 +2,34 @@
 export const sortData = (data, order) => {
   //Creando un nuevo array de data para ordenarlo sin modificar el original
   let newArrPokemon = [...data];
-  console.log ('esto es el nuevo array', newArrPokemon);
 
   //Ordenando los pokemon alfabéticamente
   let arrOrder = newArrPokemon.sort((a,b) => a.name < b.name ? -order : order) //-order a se va a posicionar antes (asc)    
     return arrOrder 
   };
 
-//Función para filtrar por tipo de pokémon
-export const filterData = (data, type) => {
+  //Función para filtrar por tipo de pokémon
+  export const filterData = (data, type) => {
   let pokemonType = data.filter((x) => {
     return x.type == type;
-  });
-  return pokemonType; // este arreglo esn de los tipo fuego
+  }) 
+  return pokemonType
 };
-
-// Esta función devuelve los pokemos fire. En main.js guardo estos valores en la const filteredData.
-
-/*
-
-//import data from "./data/pokemon/pokemon.js";
-export const sortData = (data) => {
-  //Creando un nuevo array de data para ordenarlo sin modificar el original
-  let newArrPokemon = [...data];
-  //console.log ('esto es el array original', data);
-  //console.log ('esto es el nuevo array', newArrPokemon);
-
-  //Ordenando los pokemon alfab de la A-Z
-  let arrOrderAtoZ = newArrPokemon.sort((a,b) => a.name < b.name ? -1 : 1) //-1 a se va a ordenar antes de b
-
-  //Ordenando los pokemon alfab de la Z-A
-  let arrOrderZtoA = [...newArrPokemon];
-  arrOrderZtoA.reverse()
-
-    //console.log ('esto es el array ordenado A-Z', arrOrderAtoZ)
-    //console.log ('esto es el array ordenado Z-A', arrOrderZtoA)
-    
-    //return??? 
-  }; 
-
-  // función filter
-  export const filterData = (data, type) => {
-    
-    let pokemonFire = data.filter((x) => x.type == "fire");
-    console.log("este es tipo fuego", pokemonFire);
-    
-    let pokemonEnergy = data.filter((x) => x.egg == "not in eggs");
-    console.log(pokemonEnergy);
-  };
-    
-
-
   
-
-
-/*
- else {
-      newArrPokemon.sort(function(a,b){
-        if (b.name < a.name) {
-          return -1;
-        }
-        if (b.name > a.name) {
-          return 1;
-        }
-      });
-       }
-
-
-Ordenando los objetos del array A-Z
-  function sortAToZ (a,b){
-   return a.name.localeCompare(b.name);
-  }
-let arrOrder_Az = newArrPokemon.sort(sortAToZ);
-console.log ('esto es el array ordenado A-Z', arrOrder_Az);
-
-estas funciones son de ejemplo
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-const pokemonFire = pokemons.filter((x) => x.type == "fire");
-console.log("esto es tipo fuego", pokemonFire);
-*/
+  //Función para mostrar porcentaje de pokémon por tipo
+  export const computeStats = (data, pokemonType) => {
+    let pokePercen = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].type == pokemonType) {
+        pokePercen.push(data[i].type)
+      }
+    }
+    return ((pokePercen.length) * 100 / 251).toFixed(2) + '%'
+    };
+  
+  //Función para buscar pokémon con la barra de búsqueda
+  export const searchData = (data) => {
+   let results = data.filter((x) => 
+    x.name.toLowerCase().includes(document.querySelector('#searchBar').value.toLowerCase()))
+    return results;
+  };
--- src/main.js
+++ src/main.js
@@ -1,110 +1,70 @@
-import { sortData } from "./data.js";
-import { filterData } from "./data.js";
 import data from "./data/pokemon/pokemon.js";
-
-//Nuevo código
-
-//Llamando a la sección donde estará la info de los pokémon
-let pokemonInfo = document.getElementById("pokemonInfo");
+import { sortData, filterData, computeStats, searchData } from "./data.js";
 
 //Almacenando la data en una variable
 const pokemons = data.pokemon;
-const sortedData = sortData(pokemons, 1);
- // fire entre "" porque es el string
 
-//Por cada objeto del array que muestre la info especificada
+//Llamando al div pokemonInfo
+let pokemonInfo = document.querySelector("#pokemonInfo");
 
+//Por cada objeto del array que muestre la info especificada "card"
 function showInfo(dataPokemon) {
   pokemonInfo.innerHTML = "";
   dataPokemon.forEach(function (pokemon) {
-    pokemonInfo.innerHTML += `<div class="pokemonInfo" ><img src = '${pokemon.img}' alt='foto del pokémon'> <br> <p>${pokemon.name}</p> <p>${pokemon.num}</p> <p>${pokemon.type}</p></div>`;
+    //Creamos el div pokemon info donde estarán los datos de los pokémon
+    const divPokemon = document.createElement("div");
+    //Le agregamos clase al nuevo div
+    divPokemon.setAttribute("class", "pokeInfo")
+    //Agregamos todos los datos de los pokémon al nuevo div
+    divPokemon.innerHTML += `<img src = '${pokemon.img}' alt='foto del pokémon'> <h3>${pokemon.name}</h3> <p>${pokemon.num}</p> <p>${pokemon.type}</p>`;
+    //Enviamos el div class pokeInfo al div pokemonInfo
+    pokemonInfo.appendChild(divPokemon);
+  
   });
-
 }
+// Se muestran los pokémon en el navegador
 showInfo(pokemons);
 
 //Traemos al select donde están A-Z Z-A
-let alphabeticalOrder = document.getElementById ('alphabeticalOrder');
+let pokemonOrder = document.getElementById ('alphabeticalOrder');
 
-//Al escoger una opción (change) se muestra en consola ordenados alfabéticamente
-alphabeticalOrder.addEventListener('change', function (ev) {
-let alphabeticOrder = ev.target.value
+//Al escoger una opción (change) se muestran los pokémon ordenados alfabéticamente
+pokemonOrder.addEventListener('change', function (e) {
+let alphabeticOrder = e.target.value
 const sortedData = sortData(pokemons, alphabeticOrder);
 showInfo (sortedData);
 });
 
+
 //Traemos al select donde están las opciones de tipo de pokémon
 let pokeTypes = document.getElementById ('pokeTypes');
 
-//Al escoger una opción (change) se muestra en consola los tipos de pokémon escogidos
+//Al escoger una opción (change) se muestran los tipos de pokémon escogidos
 pokeTypes.addEventListener('change', function (e) {
   let typePokemon = e.target.value 
   const filteredData = filterData(pokemons, typePokemon);
   showInfo(filteredData);
 });
 
-//*Evento change*/
-//let sectionPrueba = document.getElementById("prueba"); // atrapo el id del section id prueba
-let tipos = document.getElementById("tipos");
+//Traemos al select donde están las opciones de porcentaje de pokémon
+let pokePercentage = document.getElementById ('pokePercentage');
 
-tipos.addEventListener("change", function (e) {
-  // (e) para recibir al evento
-  let valor = e.target.value; // valor almacena  el string "water"
-  const filteredData = filterData(pokemons, valor); //es una función y devuleve un arreglo
-  showInfo(filteredData);
-  /*let soloType = [];
-  for (let i = 0; i < valor.length; i++) {
-    let valorActual = valor[i];
-    soloType.push(valorActual);
-    console.log(soloType);
-  }
+//Al escoger una opción (change) se muestran el porcentaje de pokémon escogidos
+pokePercentage.addEventListener('change', function (e) {
+let pokemonPercen = e.target.value
+const percenData = computeStats (pokemons, pokemonPercen) // Este valor es de tipo string
+console.log ('este es el porcentaje de pokémon tipo ' + pokemonPercen, percenData)
 
-  soloType.forEach(function (item) {
-    // valor no es iterable?
-    const tipo = document.createElement("p");
-    tipo.textContent = item;
-    sectionPrueba.appendChild(tipo);
-  });
-  */
 });
 
+//Traemos a la barra de búsqueda
+let searchBar = document.querySelector('#searchBar');
 
-// appendChild
-//1. Seleccio al section padre
-let sectionPrueba = document.getElementById("prueba");
-// IMAGE
-// 2. Creo el nodo img
-const imgPokemon = document.createElement("img");
-imgPokemon.src = "https://www.serebii.net/pokemongo/pokemon/001.png";
-// 3. Agrego el nodo img al section padre id prueba
-sectionPrueba.appendChild(imgPokemon);
-
-//NAME
-//1. Creo el nodo div
-const namePokemon = document.createElement("div");
-
-// 2. Agrego el nodo div al padre section prueba
-sectionPrueba.appendChild(namePokemon);
-namePokemon.style.backgroundColor = "purple";
-
-// 3. creo el texto del nombre
-const textNamePokemon1 = document.createTextNode("Aquí nombre");
-
-// 4.
-namePokemon.appendChild(textNamePokemon1);
-
-//NUMBER 
-//1. Creo el nodo div
-const numberPokemon = document.createElement("div");
-
-// 2. Agrego el nodo div al padre section prueba
-sectionPrueba.appendChild(numberPokemon);
-numberPokemon.style.backgroundColor = "red";
-
-// 3. creo el texto del nombre
-const textNamePokemon2 = document.createTextNode(pokemons.name);
-
-// 4.
-numberPokemon.appendChild(textNamePokemon2);
-
-
+//Que la barra busque los pokémon al escribir
+searchBar.addEventListener('input', () => {
+  const searchedData = searchData (pokemons)
+  showInfo(searchedData);
+  if (searchBar.value == '') {
+   showInfo(searchedData);
+  }
+});
